1c292316b321d4cc91249ab1cca65d2fc7d996b5
tluo3032 <tiantian.luo@uzh.ch>
2023-04-18 14:59:59 +0200

    Update UI

--- lottery/src/App.js
+++ lottery/src/App.js
@@ -2,19 +2,26 @@ import './styles/App.css';
 import React from "react";
 import {useState} from "react";
 import {useEffect} from "react";
-import {Button, Card, FormHelperText, Grid} from "@mui/material";
-import {Container} from "@mui/material";
-import Select, {SelectChangeEvent} from "@mui/material/Select";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import Box from "@mui/material/Box";
+import {Button, Card, FormHelperText, Grid, TextField, ThemeProvider} from "@mui/material";
 import Web3 from "web3";
 import detectEthereumProvider from '@metamask/detect-provider';
+import {createTheme} from "@mui/material";
 
 
 function App() {
     const [walletAddress, setWalletAddress]=useState();
+    const [players,setPlayers]=useState([]);
+    const [betNumber,setBetNumber]=useState(0);
+
+    const theme= createTheme({
+        palette:{
+            primary:{
+                main:'rgba(234,173,41,0.87)',
+                darker:'rgba(239,180,50,0.91)'
+            },
+        }
+    })
+
     const connectWallet = async()=>{
         const provider =await detectEthereumProvider();
         if(typeof window.ethereum !== 'undefined'){
@@ -27,77 +34,69 @@ function App() {
             console.log("MetaMask is not installed");
         }
     };
-    const [number, setNumber] = React.useState('');
 
-    const handleChange = (event: SelectChangeEvent) => {
-        setNumber(event.target.value);
-    };
+    const handleSetNumber=(value)=>{
+        setBetNumber(value);
+        console.log(betNumber);
+    }
 
     return (
       <div className="main">
-          <div className="nav">
-              <h1>Lottery dApp</h1>
-          </div>
-          <div className="row">
-              <div className="column">
-                  <div className="field">
-                      <Card sx={{minHeight:320,padding:2}}>
-                          <div className="buttonfield">
-                              <Button onClick={connectWallet} variant={"contained"} >Connect Wallet</Button>
-                          </div>
-                          <div className={"account"}>
-                              Your account address:<br/>
-                              {walletAddress}
-                          </div>
-                          <div className="select">
-                              <Box sx={{minWidth: 60}}>
-                                  <FormControl sx={{minWidth: 200}}>
-                                      <InputLabel id="demo-simple-select-label">Number</InputLabel>
-                                      <Select
-                                          labelId="demo-simple-select-helper"
-                                          id="demo-simple-select"
-                                          value={number}
-                                          label="Number"
-                                          onChange={handleChange}
-                                      >
-                                          <MenuItem value={1}>1</MenuItem>
-                                          <MenuItem value={2}>2</MenuItem>
-                                          <MenuItem value={3}>3</MenuItem>
-                                          <MenuItem value={4}>4</MenuItem>
-                                          <MenuItem value={5}>5</MenuItem>
-                                          <MenuItem value={6}>6</MenuItem>
-                                          <MenuItem value={7}>7</MenuItem>
-                                          <MenuItem value={8}>8</MenuItem>
-                                          <MenuItem value={9}>9</MenuItem>
-                                          <MenuItem value={10}>10</MenuItem>
-                                          <MenuItem value={11}>11</MenuItem>
-                                          <MenuItem value={12}>12</MenuItem>
-                                          <MenuItem value={13}>13</MenuItem>
-                                          <MenuItem value={14}>14</MenuItem>
-                                          <MenuItem value={15}>15</MenuItem>
-                                          <MenuItem value={16}>16</MenuItem>
-                                          <MenuItem value={17}>17</MenuItem>
-                                          <MenuItem value={18}>18</MenuItem>
-                                          <MenuItem value={19}>19</MenuItem>
-                                          <MenuItem value={20}>20</MenuItem>
-                                      </Select>
-                                      <FormHelperText>Choose a number between 1 and 20</FormHelperText>
-                                      <FormHelperText>Price per round:0.01 ETH</FormHelperText>
-                                  </FormControl>
-                              </Box>
-                          </div>
-                          <Button variant={"outlined"}>Play</Button>
-                      </Card>
-                  </div>
+          <div className="body">
+              <div className="title">
+                  BlockLottery
               </div>
-              <div className="column">
-                  <div className="field">
-                      <Card sx={{minHeight:320,padding:2}}>
-                          <div className="winner">
-                              <Button variant="outlined">Get Winner</Button>
-                              <h2>The winner is Sally</h2>
+              <div className="two-column-grid">
+                  <div className="left-column">
+                      <div className="info">
+                          <div className="info-text">JOIN THE LOTTERY</div>
+                          <div className="info-text-detail">Lottery game that generates winner by block</div>
+                          <div className="buttonfield">
+                              <ThemeProvider theme={theme}>
+                                  <Button onClick={connectWallet} variant={"contained"} color='primary'>Connect Wallet</Button>
+                              </ThemeProvider>
                           </div>
-                      </Card>
+                          {walletAddress && <div className={"account"}>
+                              <h6>Your account address:<br/>
+                                  {walletAddress}</h6>
+                          </div>}
+                      </div>
+
+                      <div className="interaction">
+                          <div className="field">
+                              <TextField
+                                  id="outlined-number"
+                                  value={betNumber}
+                                  label="Ether"
+                                  type="ether"
+                                  InputLabelProps={{
+                                      shrink: true,
+                                  }}
+                                  onChange={(e)=>handleSetNumber(e.target.value)}
+                              />
+
+                              <div className="buttonfield">
+                                  <ThemeProvider theme={theme}>
+                                    <Button onClick={connectWallet} variant={"contained"} >Join The Lottery</Button>
+                                  </ThemeProvider>
+                              </div>
+                          </div>
+
+                          <h3>The balance of this round:</h3>
+                          <h2>16 ETH</h2>
+
+                          <div className="buttonfield">
+                              <Button variant={"outlined"} color={"warning"}>Collect</Button>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="right-column">
+                      <div className="players">
+                          <h3>playerlist</h3>
+                          <div className="buttonfield">
+                              <Button variant={"contained"} color="warning">Get Winner</Button>
+                          </div>
+                      </div>
                   </div>
               </div>
           </div>
